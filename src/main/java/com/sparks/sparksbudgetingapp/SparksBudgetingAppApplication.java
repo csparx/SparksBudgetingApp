5d543537a157c9143d89cf7371e301f0dfb1f53e
Christy Sparks <sparkswebdev@gmail.com>
2024-12-13 10:03:22 -0600

    Initial setup for Sparks Budgeting App

---
+++ src/main/java/com/sparks/sparksbudgetingapp/SparksBudgetingAppApplication.java
@@ -0,0 +1,13 @@
+package com.sparks.sparksbudgetingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparksBudgetingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SparksBudgetingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparks/sparksbudgetingapp/SparksBudgetingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparks.sparksbudgetingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparksBudgetingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
